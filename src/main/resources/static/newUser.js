b55a79950f1d0cb3fc57cac1c8e1528ab172a518
AntonNV22 <antonnv22@gmail.com>
2020-10-24 00:05:14 +0800

    Практическая задача 3.1.3
    Задание:
    Написать Rest-контроллеры для вашего приложения.
    Переписать вывод (заполнение) таблицы, модальных окон и т.д. на JS c помощью Fetch API, допускается использование JQuery.

--- src/main/resources/static/newUser.js
+++ src/main/resources/static/newUser.js
@@ -2,10 +2,16 @@ function newUser() {
 
     let form = window.formNewUser.newRoles;
     let new_Roles = "";
+    let rolesList = document.createElement('ul');
+
     for (var i = 0; i < form.length; i++) {
         var option = form.options[i];
+        let role = document.createElement('li');
         if (option.selected) {
             new_Roles = new_Roles.concat(option.value + (i != (form.length - 1) ? "," : ""));
+
+            role.textContent = option.value + " ";
+            rolesList.appendChild(role);
         }
     }
 
@@ -22,6 +28,29 @@ function newUser() {
         headers: {"Content-type": "application/json; charset=UTF-8"}
     })
         .then(response => {
+             var tableBody = document.getElementById("tBody");
+             var row = tableBody.insertRow();
+             let id = tableBody.rows.length;
+             var cell0 = row.insertCell();
+             cell0.innerHTML = id;
+             var cell1 = row.insertCell();
+             cell1.innerHTML = window.formNewUser.newName.value;
+             var cell2 = row.insertCell();
+             cell2.innerHTML = window.formNewUser.newLastName.value;
+             var cell3 = row.insertCell();
+             cell3.innerHTML = window.formNewUser.newAge.value;
+             var cell4 = row.insertCell();
+             cell4.innerHTML = window.formNewUser.newEmail.value;
+             var cell5 = row.insertCell();
+             cell5.innerHTML = rolesList.textContent;
+             var cell6 = row.insertCell();
+             cell6.innerHTML =
+                '<button type="button" onclick="getModalEdit(' + id +')" class="btn btn-primary btn-sm">Edit</button>';
+
+             var cell7 = row.insertCell();
+             cell7.innerHTML =
+                '<button type="button" onclick="getModalDelete(' + id +')" class="btn btn-danger btn-sm">Delete</button>';
+
             window.formNewUser.newName.value = "";
             window.formNewUser.newLastName.value = "";
             window.formNewUser.newAge.value = "";
@@ -29,7 +58,8 @@ function newUser() {
             window.formNewUser.newPassword.value = "";
             window.formNewUser.newRoles.value = "";
 
-            showAllUsers();
+
+//            showAllUsers();
             $('#NewUserCreated').modal();
         });
 }
